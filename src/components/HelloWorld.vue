aec715ab641c185729521187192a7edc82e540e4
huihui036 <1315574336@qq.com>
2020-10-02 18:35:46 +0800

    vue 3.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      2020-10-02 begin test
+    </p>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo"
+         src="../assets/logo.png">
+    <HelloWorld msg="vue3.0 测试模块 " />
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
